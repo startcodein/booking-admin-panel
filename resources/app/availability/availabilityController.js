1a2fa8737a09325bf184048e2e99c8ed60de576b
Muhammed athimanil <hello@athimannil.com>
2016-10-11 23:54:17 +0200

    availability schedule without calendar

--- resources/app/availability/availabilityController.js
+++ resources/app/availability/availabilityController.js
@@ -2,60 +2,14 @@ angular
     .module('app')
     .controller('availabilityController', availabilityController);
 
-function availabilityController(moment) {
-  this.calendarView = 'month';
-  this.viewDate = new Date();
-
-  this.events = [];
-
-  this.mytime = {};
-
-  this.addEvent = function() {
-    this.events.push(this.mytime);
-    this.mytime = {};
-    // this.events.push({
-      // title: 'Monday',
-      // startsAt: moment().startOf('monday').toDate(),
-      // endsAt: moment().endOf('monday').toDate(),
-    // });
-    console.log(this.events);
-  };
-
-  this.clickOnEvent = function (calendarCell) {
-    this.mytime = {
-        "title": calendarCell.date.format('dddd'),
-        // TODO set start-time if there another time
-        "startsAt" : moment().startOf('day'),
-        // TODO set end-time if there is another time
-        "endsAt" : moment().endOf('day')
-    }
+function availabilityController() {
+  this.schedules = {
+    "MON":[{"start":"08:00:00","end":"10:30:00"},{"start":"11:00:00","end":"13:30:00"},{"start":"14:00:00","end":"16:00:00"}],
+    "TUE":[{"start":"10:00:00","end":"10:30:00"},{"start":"11:00:00","end":"13:30:00"},{"start":"14:00:00","end":"16:00:00"}],
+    "WED":[{"start":"23:00:00","end":"10:30:00"},{"start":"11:00:00","end":"13:30:00"},{"start":"14:00:00","end":"16:00:00"}],
+    "THU":[{"start":"10:00:00","end":"10:30:00"},{"start":"11:00:00","end":"13:30:00"},{"start":"14:00:00","end":"16:00:00"}],
+    "FRI":[{"start":"10:00:00","end":"10:30:00"},{"start":"11:00:00","end":"13:30:00"},{"start":"14:00:00","end":"16:00:00"}],
+    "SAT":[{"start":"10:00:00","end":"10:30:00"},{"start":"11:00:00","end":"13:30:00"},{"start":"14:00:00","end":"16:00:00"}],
+    "Sun":[{"start":"10:00:00","end":"10:30:00"},{"start":"11:00:00","end":"13:30:00"},{"start":"14:00:00","end":"16:00:00"}],
   }
-  this.startOption = {
-    "mstep" : 5,
-    // TODO set start-time if there another time
-    "startTime": moment().startOf('day'),
-    // TODO set end-time if there is another time
-    "endTime": moment().endOf('day')
-  };
-  this.endOption = {
-    "mstep" : 5,
-    // TODO set start-time if there another time
-    "startTime": moment().hours(2).minutes(8),
-    // TODO set end-time if there is another time
-    "endTime": moment().hours(23).minutes(10)
-  };
-  this.updateChangeStart = function () {
-    // TODO update end-time if start-time more then end-time
-    // this.mytime.endsAt = this.mytime.startsAt;
-    // console.log(this.mytime.endsAt);
-      if(moment(this.mytime.startsAt).isSameOrAfter(moment(this.mytime.endsAt))){
-        console.log("event can not start after exceed");
-      }else{
-        console.log("thudangi kayinj avasanikunu");
-      }
-    //console.log(moment('2010-10-20').isSameOrBefore(moment('2010-10-21')));
-  };
-  this.updateChangeEnd = function () {
-    // TODO set min="" to same start-time
-  };
 }
